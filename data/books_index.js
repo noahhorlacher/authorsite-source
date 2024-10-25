5cd5355e9e3db6eab3c695ade309c75f74108c8e
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-25 12:09:19 +0200

    added book

--- data/books_index.js
+++ data/books_index.js
@@ -1,9 +1,11 @@
 import { gedichteUndBestaunungen } from './books/gedichte-und-bestaunungen'
+import { derMechanischeEngel } from './books/der-mechanische-engel'
 import { leonhardMondsturmDieRacheDesUdohr } from './books/leonhard-mondsturm-die-rache-des-udohr'
 import { leonhardMondsturmDasHerzDesWaldes } from './books/leonhard-mondsturm-das-herz-des-waldes'
 
 export const booksIndex = [
-    gedichteUndBestaunungen,
+    leonhardMondsturmDasHerzDesWaldes,
     leonhardMondsturmDieRacheDesUdohr,
-    leonhardMondsturmDasHerzDesWaldes
+    derMechanischeEngel,
+    gedichteUndBestaunungen,
 ]
