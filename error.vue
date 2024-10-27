587c84c9b3bbbaa2c50b7e81f5207f93ceddad32
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-27 06:25:09 +0100

    removed unnecessary css

--- error.vue
+++ error.vue
@@ -13,12 +13,3 @@
         </div>
     </div>
 </template>
-  
-  <style scoped>
-  .button {
-    padding: 4px 6px;
-    margin: 10px 0px;
-    background: black;
-    color: white;
-  }
-  </style>
