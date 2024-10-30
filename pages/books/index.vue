986e74954740e481a292ba68253505e29697a50c
Noah Horlacher <kuromarumusic@gmail.com>
2024-10-30 21:45:26 +0100

    added more gap

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -27,7 +27,7 @@ function sortBooks(booksData){
 <template>
     <n-section class="bg-gray-100">
         <h2 class="text-4xl font-bold mb-4 text-center mt-16">Alle Bücher</h2>
-        <div class="flex justify-center py-16 gap-16 flex-wrap">
+        <div class="flex justify-center py-16 gap-16 md:gap-32 flex-wrap">
             <n-book v-for="book in books" :book="book" />
         </div>
     </n-section>
